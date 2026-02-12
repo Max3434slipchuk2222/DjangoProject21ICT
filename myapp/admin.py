f2d9757802b847ef289b9a1e09f1f4462d0a17bb
Max Slipchuk <maxslipchuk11@gmail.com>
2026-02-13 00:13:16 +0200

    lab4

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Teacher, Course, Student
 
-# Register your models here.
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'created_at', 'updated_at')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'created_at', 'updated_at')
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Student, StudentAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    courses = models.ManyToManyField(Course)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
