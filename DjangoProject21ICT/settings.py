de185478c8b2e105537edd886fc22e4626a3e238
Max Slipchuk <maxslipchuk11@gmail.com>
2026-03-20 00:26:05 +0200

    update DataBase and Images

--- DjangoProject21ICT/settings.py
+++ DjangoProject21ICT/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'DjangoProject21ICT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'ep-late-sea-amhsunc1-pooler.c-5.us-east-1.aws.neon.tech',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_QkO7nauzRHv5',
+        'PORT': '5432'
     }
 }
 
@@ -123,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django_resized import ResizedImageField
+
 
 class Teacher(models.Model):
     full_name = models.CharField(max_length=100)
@@ -20,7 +22,14 @@ class Course(models.Model):
     description = models.TextField(blank=True, default='')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='courses', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/', blank=True, null=True)
+    image = ResizedImageField(
+        size=[800, 600],
+        quality=85,
+        force_format='WEBP',
+        upload_to='courses_images/',
+        null=True,
+        blank=True
+    )
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
